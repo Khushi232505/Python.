01fb2a1158361cffad19e09c2f8e5b298f67c209
Khushi232505 <khushiitiwari23@gmail.com>
2025-12-20 14:41:37 +0530

    Create 04_operators.py

---
+++ Chapter-2/04_operators.py
@@ -0,0 +1,36 @@
+# Arithmetic operators
+a = 5
+b = 7
+c = a*b
+print(c)
+
+# Assignment Opearators
+a = 4-3 # Assign 4-3 in a
+print(a)
+b = 8
+b += 5 #Increment the value of b by 5 and then assign it to b
+b -= 5 #decrement the value of b by 5 and then assign it to b
+print(b)
+
+# Comparison Operators
+d = 5>=4
+dd = 5!=5
+print(d)
+print(dd)  # "==" used for comparison and  "= "is used for assigning values
+
+# Logical Operators
+e = True or False
+
+# Truth Table of "OR"
+print("True or False is ", True or False)
+print("True or True is ", True or True)
+print("False or True is ", False or True)
+print("False or False is ", False or False)
+
+# Truth Table of "AND"
+print("True and False is ", True and False)
+print("True and True is ", True and True)
+print("False and True is ", False and True)
+print("False and False is ", False and False)
+
+print (not (False))
