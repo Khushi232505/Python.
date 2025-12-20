8f243b1c6d8cff88e5188bfae92c7ee481a3d2d7
Khushi232505 <khushiitiwari23@gmail.com>
2025-12-20 14:37:33 +0530

    Create 03_rules_variables.py

---
+++ Chapter-2/03_rules_variables.py
@@ -0,0 +1,13 @@
+a = 34
+
+aaa = 99
+
+harry = 78
+
+_harry = 67
+
+# @khushi = 89 only start with underscore and alphabet
+
+# 67herceh = 78 can't start with a number or a special character
+
+# yuu oi4d = 8999 cant provide space in variable name
