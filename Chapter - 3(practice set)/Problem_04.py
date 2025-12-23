c70c4c5b33a8028c193089dab1d79177ee60ab59
Khushi232505 <khushiitiwari23@gmail.com>
2025-12-23 22:42:43 +0530

    Create Problem_04.py

---
+++ Chapter - 3(practice set)/Problem_04.py
@@ -0,0 +1,7 @@
+name = "Khushi  is  a good  girl and"
+
+print(name.replace("  ", " ")) 
+# STRINGS are immutable which means you cant chnag them by running function on themname = "Khushi  is  a good  girl and"
+
+print(name.replace("  ", " ")) 
+# STRINGS are immutable which means you cant chnag them by running function on them
