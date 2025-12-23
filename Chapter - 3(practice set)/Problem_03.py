8e540708045c40ae41b961c2521b366ed8f8c79a
Khushi232505 <khushiitiwari23@gmail.com>
2025-12-23 22:42:01 +0530

    Create Problem_03.py

---
+++ Chapter - 3(practice set)/Problem_03.py
@@ -0,0 +1,5 @@
+name = "Khushi is a good  girl and  "
+
+print(name.find("  "))
+
+# if the output is -1 t hat means that the string do not have any double spaces but if the output is some other number than it means that there is double space in the given string
