26519a4b5111463e988ed58a5a2337ab0bb12f07
Khushi232505 <khushiitiwari23@gmail.com>
2025-12-25 20:00:00 +0530

    Create 01_list.py

---
+++ Chapter - 4/01_list.py
@@ -0,0 +1,8 @@
+friends = ["Apple", "Orange", 5, 345.06, False,  "Aakash", "Rohan"]
+
+print(friends[0])
+friends[1] = "Grapes"
+
+print(friends[1])
+print(friends[1:5]) # List Slicing
+# Unlike Strings Lists are mutable
