962e1162fde52a871ee6c418181f34d51f4e67a9
Khushi232505 <khushiitiwari23@gmail.com>
2025-12-28 22:41:33 +0530

    Create 04_set_methods.py

---
+++ Chapter - 5/04_set_methods.py
@@ -0,0 +1,13 @@
+s = {1, 5, 32, 54, 5, 5, 5, "harry"}
+
+print(s, type(s))
+
+s.add(566)
+print(s, type(s)) #add the element in the set
+
+s.remove(1) # update the set s and removes 1 from the set
+print(s)
+
+s.clear()
+print(s)
+
