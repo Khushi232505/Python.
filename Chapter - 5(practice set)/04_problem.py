0f6a20f78ab8fa5894666885b3e6d384066dbce1
Khushi232505 <khushiitiwari23@gmail.com>
2025-12-29 21:58:06 +0530

    Create 04_problem.py

---
+++ Chapter - 5(practice set)/04_problem.py
@@ -0,0 +1,10 @@
+# 4. What will be the length of following set s:
+s = set()
+s.add(20)
+s.add(20.0)
+s.add('20') # length of s after these operations?
+ 
+print(s)
+print(len(s))
+
+# the comparison operator sees if the value is equal or not irespective of the datatype 
