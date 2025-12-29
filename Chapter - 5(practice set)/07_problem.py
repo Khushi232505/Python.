d69afee23c16df85f7780e976cc6d3c3445b0dca
Khushi232505 <khushiitiwari23@gmail.com>
2025-12-29 22:01:19 +0530

    Create 07_problem.py

---
+++ Chapter - 5(practice set)/07_problem.py
@@ -0,0 +1,23 @@
+# If the names of 2 friends are same; what will happen to the program in problem 6?
+
+d = {}
+
+name = input("Enter friends name: ")
+lang = input("Enter the language name: ")
+d.update({name : lang})
+
+name = input("Enter friends name: ")
+lang = input("Enter the language name: ")
+d.update({name : lang})
+
+name = input("Enter friends name: ")
+lang = input("Enter the language name: ")
+d.update({name : lang})
+
+name = input("Enter friends name: ")
+lang = input("Enter the language name: ")
+d.update({name : lang})
+
+print(d)
+
+# It will update the dictionary whatever you will write at end will be print in dictionary
