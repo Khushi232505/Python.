586c700f1796d6f65721fe587b06e17ab6923ae3
Khushi232505 <khushiitiwari23@gmail.com>
2025-12-30 22:46:37 +0530

    Create 01_conditionals.py

---
+++ Chapter - 6/01_conditionals.py
@@ -0,0 +1,15 @@
+# In python programming too, we must be able to execute instructions on a condition(s) being met.
+
+# Whenever we write if/else/elif condition and go to next line  our cursor leaves some space which is called IDENTATION..
+
+a = int(input("Enter your age: "))
+
+# If else statement
+
+if(a>18):
+    print("You are above the age of consent")
+
+else :
+    print("You are below the age of consent")
+
+print("End of program")
