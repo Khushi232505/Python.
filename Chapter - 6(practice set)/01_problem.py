fe795b4b78d5f6ba9991674d46465894bc8d3111
Khushi232505 <khushiitiwari23@gmail.com>
2025-12-31 23:16:04 +0530

    Create 01_problem.py

---
+++ Chapter - 6(practice set)/01_problem.py
@@ -0,0 +1,18 @@
+a = int(input("Enter the first number : "))
+b = int(input("Enter the second number : "))
+c = int(input("Enter the third number : "))
+d = int(input("Enter the fourth number : "))
+
+if (a>b and a>c and a>d):
+    print("Greatest number is a: ", a)
+    
+elif (b>a and b>c and b>d):
+    print ("Greatest number is b: ", b)
+    
+elif (c>a and c>b and c>d):
+    print("Greatest number is d: ", c)
+    
+else:
+    print("Greatest number is d: ", d)
+    
+
