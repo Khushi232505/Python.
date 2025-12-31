69d450a3f27c1e7539b4bcfe91915bb6aae732f0
Khushi232505 <khushiitiwari23@gmail.com>
2025-12-31 23:18:46 +0530

    Create 05_escape_seq.py

---
+++ Chapter - 3/05_escape_seq.py
@@ -0,0 +1,8 @@
+a = "harry \'is\' a \\ good boy \n but \"not\"  a bad \t boy"
+print(a)
+
+# \n is used for adding new line or going to next line
+# \t is used to add gap between the words
+# \" is used for adding double quotes inside the double quotes
+# \' is used for adding single quotes inside the single quotes
+# \\ used for adding backslash(/)
