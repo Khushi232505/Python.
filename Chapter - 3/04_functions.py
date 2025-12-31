8ab4926f037bbc013a5592b9b886e896d3935ec6
Khushi232505 <khushiitiwari23@gmail.com>
2025-12-31 23:17:13 +0530

    Delete Chapter - 3/04_functions.py

--- Chapter - 3/04_functions.py
+++
@@ -1,9 +0,0 @@
-name = "yuvraj hii"
-
-print(len(name))
-print(name.endswith("raj"))
-print(name.startswith("yuv"))
-print(name.capitalize()) # only the first word will be in capital letter not the whole word
-print(name.upper())
-print(name.lower())
-print(name.title())
