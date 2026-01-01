7942c83100a80ccf2be18d6617f49f1854d301b5
Khushi232505 <khushiitiwari23@gmail.com>
2026-01-01 23:39:26 +0530

    Create 03_problem.py

---
+++ Chapter - 6(practice set)/03_problem.py
@@ -0,0 +1,10 @@
+ color = input("enter color: ")
+
+if color == "red":
+    print("stop")
+elif color == "yellow":
+    print("look")
+elif color == "green":
+    print("go")
+else :
+    print("wrong color for traffic lights")
