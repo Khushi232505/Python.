8f7eb4b13a4c3cc617d983a90705b77df1867f1a
Khushi232505 <khushiitiwari23@gmail.com>
2026-01-02 23:58:37 +0530

    Create module.py

---
+++ Chapter-1/module.py
@@ -0,0 +1,12 @@
+import pyjokes
+
+# print("Printing Jokes...")
+joke = pyjokes.get_joke()
+print(joke)
+
+"""hii
+khushi
+how are you
+bhappppp"""
+
+# we use ctrl+/ (#) in order to comment any line and we use """"""" for multi line comment
