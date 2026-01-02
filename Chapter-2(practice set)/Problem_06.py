a3d823a6821587630ec7d98d42864c710f3a45c0
Khushi232505 <khushiitiwari23@gmail.com>
2026-01-02 23:59:37 +0530

    Delete Chapter-2(practice set)/Problem_06.py

--- Chapter-2(practice set)/Problem_06.py
+++
@@ -1,5 +0,0 @@
-a = int(input("Enter the number: "))
-sqr = a*a
-# sqr = a**a # This is valid
-# sqr =  a^2 #Incorrect for finding square of a number in Python
-print("The square of the given number is: ", sqr)
