0a7cc55a9d803657a6c908320f3943b3e77b1eb6
Khushi232505 <khushiitiwari23@gmail.com>
2026-01-05 22:43:01 +0530

    Create 05_problem.py

---
+++ Chapter - 6(practice set)/05_problem.py
@@ -0,0 +1,10 @@
+# Write a program which finds out whether a given name is present in a list or not.
+
+l = ["arpit", "shaurya", "shivansh", "devansh"]
+
+name = input("enter the name : ")
+
+if (name in l):
+    print("name is present in the list")
+else:
+    print("name is not present")
