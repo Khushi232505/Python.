7cb11c4928db0fe3449141c2754cd6351ba1e225
Khushi232505 <khushiitiwari23@gmail.com>
2026-01-06 20:13:45 +0530

    Create 08_problem.py

---
+++ Chapter - 6(practice set)/08_problem.py
@@ -0,0 +1,9 @@
+# Print if any number is odd or even
+
+n = float(input("enter number: "))
+
+if (n % 2 == 0):
+    print("the given number is even")
+else :
+    print("the given number is odd")
+    
