a1af3522e1d4cb5441309c60f5a3d2ef1d8e8f92
Khushi232505 <khushiitiwari23@gmail.com>
2026-01-06 20:14:59 +0530

    Delete Chapter - 6(practice set)/10_problem.py

--- Chapter - 6(practice set)/10_problem.py
+++
@@ -1,9 +0,0 @@
-username = input("enter the username: ")
-password = input("enter the password: ")
-
-if (username == "admin" and password == "pass") :
-    print("you have sucessfully loggedin")
-elif (username != "admin"):
-    print("wrong username")
-else :
-    print("wrong password")
