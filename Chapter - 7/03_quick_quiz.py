3de47882471b469e4c1109a4670a06ae866236df
Khushi232505 <khushiitiwari23@gmail.com>
2026-01-09 23:24:38 +0530

    Create 03_quick_quiz.py

---
+++ Chapter - 7/03_quick_quiz.py
@@ -0,0 +1,17 @@
+# Write a program to print 1 to 50 using a while loop.
+
+'''i = 1
+
+while(i<51):
+    print(i)
+    i += 1 '''
+    
+# Write a program to print the content of a list using while loops.
+
+l = [1, "harry", False, "shubhi", "this", "rohan", "shubham"]
+
+i = 0
+while(i<len(l)):
+    print(l[i])
+    i += 1 
+    
